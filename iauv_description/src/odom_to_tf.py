698bc780973e57d9704e7ffe244c1ec900e9b3f4
GitSRealpe <msn-realpe@hotmail.com>
2024-09-30 13:53:50 +0200

    minor edits

--- iauv_description/src/odom_to_tf.py
+++ iauv_description/src/odom_to_tf.py
@@ -4,27 +4,43 @@ import tf
 from nav_msgs.msg import Odometry
 
 
-# This node is used to publish tf based on the odometry of the vehicle
-# as the Girona1000 publishes the tf through the driver of the INS
-def callback(msg):
-    br = tf.TransformBroadcaster()
-    br.sendTransform(
-        (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z),
-        (
-            msg.pose.pose.orientation.x,
-            msg.pose.pose.orientation.y,
-            msg.pose.pose.orientation.z,
-            msg.pose.pose.orientation.w,
-        ),
-        rospy.Time.now(),
-        "/girona1000/base_link",
-        "/world_ned",
-    )
+class Odom_translator:
+    param = "nothing"
+
+    def __init__(self):
+        self.param = rospy.get_param("~name")
+        print("subscribing to" + self.param + "odometry")
+        rospy.Subscriber(
+            "/" + self.param + "/dynamics/odometry", Odometry, self.callback
+        )
+
+    # This node is used to publish tf based on the odometry of the vehicle
+    # as the robot publishes the tf through the driver of the INS
+    def callback(self, msg):
+        br = tf.TransformBroadcaster()
+        br.sendTransform(
+            (
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+            ),
+            (
+                msg.pose.pose.orientation.x,
+                msg.pose.pose.orientation.y,
+                msg.pose.pose.orientation.z,
+                msg.pose.pose.orientation.w,
+            ),
+            rospy.Time.now(),
+            "/" + self.param + "/base_link",
+            "/world_ned",
+        )
 
 
 def listener():
     rospy.init_node("odom_to_tf", anonymous=True)
-    rospy.Subscriber("/girona1000/dynamics/odometry", Odometry, callback)
+
+    odomt = Odom_translator()
+
     rospy.spin()
 
 
