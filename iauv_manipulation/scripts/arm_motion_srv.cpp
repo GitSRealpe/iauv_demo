f50c13fe93bc4f4dbafae4096af7e1415550927a
GitSRealpe <msn-realpe@hotmail.com>
2024-12-09 18:37:31 +0100

    improvements

--- iauv_manipulation/scripts/arm_motion_srv.cpp
+++ iauv_manipulation/scripts/arm_motion_srv.cpp
@@ -10,52 +10,83 @@ class arm_motion_srv
 private:
     ros::NodeHandle nh;
     ros::ServiceServer motion_srv;
-    actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac;
+
+    std::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> ac;
 
     std::string srv_name;
+    std::string ctrl;
     std::string node_name;
 
 public:
     arm_motion_srv(/* args */);
     ~arm_motion_srv();
 
-    bool motion_request(iauv_manipulation::Motion::Request &req, iauv_manipulation::Motion::Response &res);
+    bool motion_request(iauv_intervention_msgs::Motion::Request &req, iauv_intervention_msgs::Motion::Response &res);
 };
 
-arm_motion_srv::arm_motion_srv(/* args */) : ac("/girona1000/bravo/joint_trajectory_controller/follow_joint_trajectory", true)
+arm_motion_srv::arm_motion_srv(/* args */)
 {
     ROS_INFO("Initializing arm motions service");
 
     node_name = ros::this_node::getName();
     nh.getParam(node_name + "/srv_name", srv_name);
+    motion_srv = nh.advertiseService(srv_name, &arm_motion_srv::motion_request, this);
 
-    motion_srv = nh.advertiseService("/arm_motion_srv", &arm_motion_srv::motion_request, this);
-
+    nh.getParam(node_name + "/controller", ctrl);
+    ac = std::make_shared<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>>(ctrl + "/follow_joint_trajectory", true);
     ROS_INFO("Waiting for arm controler action server to start.");
     // wait for the action server to start
-    ac.waitForServer(); // will wait for infinite time
+    ac->waitForServer();
     ROS_INFO("Arm controler action server active.");
+
+    XmlRpc::XmlRpcValue motions;
+    nh.getParam("motions/", motions);
+    for (int i = 0; i < motions.size(); i++)
+    {
+        std::cout << motions[i]["motion_name"] << "\n";
+    }
 }
 
 arm_motion_srv::~arm_motion_srv()
 {
-    ROS_INFO("Finishin arm motions service");
-    ac.cancelAllGoals();
+    ROS_INFO("Finishing arm motions service");
+    ac->cancelAllGoals();
     // ac.~SimpleActionClient();
 }
 
-bool arm_motion_srv::motion_request(iauv_manipulation::Motion::Request &req, iauv_manipulation::Motion::Response &res)
+bool arm_motion_srv::motion_request(iauv_intervention_msgs::Motion::Request &req, iauv_intervention_msgs::Motion::Response &res)
 {
     ROS_INFO("Requested [%s] arm motion", req.request.c_str());
 
+    XmlRpc::XmlRpcValue motions;
+    nh.getParam("motions/", motions);
+    int m = 0;
+    for (; m < motions.size(); m++)
+    {
+        if (motions[m]["motion_name"] == req.request)
+        {
+            std::cout << "found: " << motions[m]["motion_name"] << "\n";
+            break;
+        }
+    }
+
+    if (m == motions.size())
+    {
+        std::cout << "motion not found bro" << "\n";
+        res.response = "motion not found in config file";
+        return false;
+    }
+
     control_msgs::FollowJointTrajectoryGoal joint_cmd;
+    for (int i = 0; i < motions[m]["joint_names"].size(); i++)
+    {
+        joint_cmd.trajectory.joint_names.push_back(motions[m]["joint_names"][i]);
+    }
 
-    nh.getParam("motions/" + req.request + "/joint_names", joint_cmd.trajectory.joint_names);
-
-    XmlRpc::XmlRpcValue points;
-    nh.getParam("motions/" + req.request + "/points", points);
+    XmlRpc::XmlRpcValue points = motions[m]["points"];
     std::cout << points.size() << "\n";
     joint_cmd.trajectory.points.resize(points.size());
+
     for (int i = 0; i < points.size(); i++)
     {
         XmlRpc::XmlRpcValue positions = points[i]["positions"];
@@ -75,9 +106,9 @@ bool arm_motion_srv::motion_request(iauv_manipulation::Motion::Request &req, iau
 
     std::cout << joint_cmd << "\n";
 
-    ac.sendGoal(joint_cmd);
+    ac->sendGoal(joint_cmd);
 
-    bool finished = ac.waitForResult(ros::Duration(10.0));
+    bool finished = ac->waitForResult(ros::Duration(10.0));
 
     res.response = finished ? "motion done" : "motion not done";
 
